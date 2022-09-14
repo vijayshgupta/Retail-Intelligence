70c2446b86bdc4c7921dec1028461d3b0ebaa39a
Vijay Shankar Gupta <vijayshgupta@gmail.com>
2022-09-14 17:52:45 +0530

    Update rb_app.py

--- rb_app.py
+++ rb_app.py
@@ -49,13 +49,13 @@ else:
   upload_url = ''.join([
       #'https://detect.roboflow.com/',
       #'?model=retail_dataset&version=1&api_key=lKyG2oSGXzoyvK2Zsjkj',
-      'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-      '?access_token=vbIBKNgIXqAQ',
+      'https://infer.roboflow.com/retail_dataset',
+      '?access_token=lKyG2oSGXzoyvK2Zsjkj',
       '&format=image',
       f'&overlap={overlap_threshold * 100}',
       f'&confidence={confidence_threshold * 100}',
       '&stroke=3',
-      '&labels=false'
+      '&labels=True'
   ])
 
   ## POST to the API.
@@ -77,10 +77,10 @@ else:
 
   ## Construct the URL to retrieve JSON.
   upload_url = ''.join([
-      'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-      '?access_token=vbIBKNgIXqAQ'
-      #'https://detect.roboflow.com/',
-      #'?model=retail_dataset&version=1&api_key=lKyG2oSGXzoyvK2Zsjkj'
+      #'https://infer.roboflow.com/rf-bccd-bkpj9--1',
+      #'?access_token=vbIBKNgIXqAQ'
+      'https://infer.roboflow.com/retail_dataset',
+      '?access_token=lKyG2oSGXzoyvK2Zsjkj'
   ])
 
   ## POST to the API.
