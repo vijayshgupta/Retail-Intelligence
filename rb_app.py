95b8ee554b0aabbc5c956a5874ae65b42a46182e
Vijay Shankar Gupta <vijayshgupta@gmail.com>
2022-09-14 19:29:10 +0530

    Update rb_app.py

--- rb_app.py
+++ rb_app.py
@@ -15,90 +15,112 @@ import matplotlib.pyplot as plt
 
 # Add in location to select image.
 
-st.title("Image Analytics for Retail Intelligence  ")
-st.write("SKU Item Detection using Computer Vision : Demo")
-file = st.file_uploader("Please upload an image file", type=["jpg","jpeg","png",'tif'])
+st.sidebar.write('#### Select an image to upload.')
+uploaded_file = st.sidebar.file_uploader('',
+                                         type=['png', 'jpg', 'jpeg'],
+                                         accept_multiple_files=False)
 
+st.sidebar.write('[Find additional images on Roboflow.](https://public.roboflow.com/object-detection/bccd/)')
 
 ## Add in sliders.
 confidence_threshold = st.sidebar.slider('Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0, 0.5, 0.01)
 overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
 
 
+image = Image.open('./images/roboflow_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
+
+image = Image.open('./images/streamlit_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
+
 ##########
 ##### Set up main app.
 ##########
 
-if file is None:
-    st.text("Please upload an image file")
+## Title.
+st.write('# Blood Cell Count Object Detection')
+
+## Pull in default image or user-selected image.
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
+
 else:
-  image = Image.open(file)
-  
-  ## Subtitle.
-  st.write('### Inferenced Image')
+    # User-selected image.
+    image = Image.open(uploaded_file)
 
-  # Convert to JPEG Buffer.
-  buffered = io.BytesIO()
-  image.save(buffered, quality=100, format='JPEG')
+## Subtitle.
+st.write('### Inferenced Image')
 
-  # Base 64 encode.
-  img_str = base64.b64encode(buffered.getvalue())
-  img_str = img_str.decode('ascii')
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
 
-  ## Construct the URL to retrieve image.
-  upload_url = ''.join([
-      f'https://detect.roboflow.com/retail-sku110/4',
-      f'?api_key=9uGj14Y2zTQoUwsMhPSu',
-      '&format=image',
-      '&overlap={overlap_threshold * 100}',
-      '&confidence={confidence_threshold * 100}',
-      '&stroke=7'
-  ])
+# Base 64 encode.
+img_str = base64.b64encode(buffered.getvalue())
+img_str = img_str.decode('ascii')
 
-  ## POST to the API.
-  r = requests.post(upload_url,
-                    data=img_str,
-                    headers={
-      'Content-Type': 'application/x-www-form-urlencoded'
-  })
+## Construct the URL to retrieve image.
+upload_url = ''.join([
+    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
+    '?access_token=vbIBKNgIXqAQ',
+    '&format=image',
+    f'&overlap={overlap_threshold * 100}',
+    f'&confidence={confidence_threshold * 100}',
+    '&stroke=2',
+    '&labels=True'
+])
 
-  image = Image.open(BytesIO(r.content))
+## POST to the API.
+r = requests.post(upload_url,
+                  data=img_str,
+                  headers={
+    'Content-Type': 'application/x-www-form-urlencoded'
+})
 
-  # Convert to JPEG Buffer.
-  buffered = io.BytesIO()
-  image.save(buffered, quality=90, format='JPEG')
+image = Image.open(BytesIO(r.content))
 
-  # Display image.
-  st.image(image,
-           use_column_width=True)
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
 
-  ## Construct the URL to retrieve JSON.
-  upload_url = ''.join([
-      f'https://detect.roboflow.com/retail-sku110/4',
-      f'?api_key=9uGj14Y2zTQoUwsMhPSu'
-  ])
+# Display image.
+st.image(image,
+         use_column_width=True)
 
-  ## POST to the API.
-  r = requests.post(upload_url,
-                    data=img_str,
-                    headers={
-      'Content-Type': 'application/x-www-form-urlencoded'
-  })
+## Construct the URL to retrieve JSON.
+upload_url = ''.join([
+    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
+    '?access_token=vbIBKNgIXqAQ'
+])
 
-  ## Save the JSON.
-  output_dict = r.json()
+## POST to the API.
+r = requests.post(upload_url,
+                  data=img_str,
+                  headers={
+    'Content-Type': 'application/x-www-form-urlencoded'
+})
 
-  ## Generate list of confidences.
-  confidences = [box['confidence'] for box in output_dict['predictions']]
+## Save the JSON.
+output_dict = r.json()
 
-  ## Summary statistics section in main app.
-  st.write('### Summary Statistics')
-  st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
-  st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
+## Generate list of confidences.
+confidences = [box['confidence'] for box in output_dict['predictions']]
 
-  ## Histogram in main app.
-  st.write('### Histogram of Confidence Levels')
-  fig, ax = plt.subplots()
-  ax.hist(confidences, bins=10, range=(0.0,1.0))
-  st.pyplot(fig)
+## Summary statistics section in main app.
+st.write('### Summary Statistics')
+st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
+st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
 
+## Histogram in main app.
+st.write('### Histogram of Confidence Levels')
+fig, ax = plt.subplots()
+ax.hist(confidences, bins=10, range=(0.0,1.0))
+st.pyplot(fig)
+
+## Display the JSON in main app.
+st.write('### JSON Output')
+st.write(r.json())
