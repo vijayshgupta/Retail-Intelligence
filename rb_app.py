c3bdeb182477b8d6fc386b7fe5c70eb75a087f87
Vijay Shankar Gupta <vijayshgupta@gmail.com>
2022-09-14 19:36:46 +0530

    Update rb_app.py

--- rb_app.py
+++ rb_app.py
@@ -27,13 +27,6 @@ confidence_threshold = st.sidebar.slider('Confidence threshold: What is the mini
 overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
 
 
-image = Image.open('./images/roboflow_logo.png')
-st.sidebar.image(image,
-                 use_column_width=True)
-
-image = Image.open('./images/streamlit_logo.png')
-st.sidebar.image(image,
-                 use_column_width=True)
 
 ##########
 ##### Set up main app.
@@ -44,9 +37,7 @@ st.write('# Blood Cell Count Object Detection')
 
 ## Pull in default image or user-selected image.
 if uploaded_file is None:
-    # Default image.
-    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
-    image = Image.open(requests.get(url, stream=True).raw)
+    st.write('Select an image to upload.')
 
 else:
     # User-selected image.
@@ -57,7 +48,7 @@ st.write('### Inferenced Image')
 
 # Convert to JPEG Buffer.
 buffered = io.BytesIO()
-image.save(buffered, quality=90, format='JPEG')
+image.save(buffered, quality=100, format='JPEG')
 
 # Base 64 encode.
 img_str = base64.b64encode(buffered.getvalue())
@@ -121,6 +112,3 @@ fig, ax = plt.subplots()
 ax.hist(confidences, bins=10, range=(0.0,1.0))
 st.pyplot(fig)
 
-## Display the JSON in main app.
-st.write('### JSON Output')
-st.write(r.json())
